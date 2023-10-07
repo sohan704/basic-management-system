c82c2969ed54940ec120d004cad32eb2b2538257
sohan704 <aridsohan005@gmail.com>
2023-10-07 23:43:23 +0600

    added error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,23 @@
+import Navbar from "../Components/Navbar";
+
 const Error = () => {
   return (
-    <div className="mt-24 text-red-500 text-center text-7xl font-semibold">
-        ERROR! 404
-    </div>
+     <div>
+
+      <Navbar></Navbar>
+         
+      <div className="mt-10">
+        <img className="mx-auto" src="https://i.ibb.co/3rvHR0X/reddit404d.png" alt="" />
+      </div>
+
+      <div className="mt-10 text-red-500 text-center text-4xl font-semibold">
+        Page not found
+        </div>
+      <div className="mt-5 text-red-500 text-center text-xl font-md">
+        The page you requested does not exist
+        </div>
+     </div>
+    
   );
 };
 
