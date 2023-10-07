4720d549014bba6e69173f34f3c1426b2c393123
sohan704 <aridsohan005@gmail.com>
2023-10-08 01:39:00 +0600

    final minor change

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -4,16 +4,17 @@ import { AuthContext } from "../Provider/AuthProvider";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { updateProfile } from "firebase/auth";
+import Navbar from "../Components/Navbar";
 
 
 
 
 const Registration = () => {
-  
-  const {createUser} = useContext(AuthContext);
+
+  const { createUser } = useContext(AuthContext);
 
 
-   
+
 
   const handleSignUp = (e) => {
     e.preventDefault();
@@ -22,9 +23,9 @@ const Registration = () => {
     const email = e.target.email.value;
     const password = e.target.password.value;
 
-    if(password.length < 6){
+    if (password.length < 6) {
       console.log('Password too short');
-      
+
       toast.error('Password too short!', {
         position: "top-right",
         autoClose: 5000,
@@ -34,13 +35,13 @@ const Registration = () => {
         draggable: true,
         progress: undefined,
         theme: "light",
-        });
+      });
       return;
     }
-    else if(!/[A-Z]/.test(password)){
+    else if (!/[A-Z]/.test(password)) {
       console.log('At least one uppercase required');
-     
-      
+
+
       toast.error('At least one uppercase required!', {
         position: "top-right",
         autoClose: 5000,
@@ -50,12 +51,12 @@ const Registration = () => {
         draggable: true,
         progress: undefined,
         theme: "light",
-        });
+      });
 
       return;
     }
-    else if(!/^(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*.]+$/.test(password)){
-     
+    else if (!/^(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*.]+$/.test(password)) {
+
       toast.error('please use special character!', {
         position: "top-right",
         autoClose: 5000,
@@ -65,24 +66,24 @@ const Registration = () => {
         draggable: true,
         progress: undefined,
         theme: "light",
-        });
+      });
       return;
     }
 
-    createUser(email,password)
-    .then(result => {
-     
+    createUser(email, password)
+      .then(result => {
 
-      updateProfile(result.user, {
-        displayName: name,
-        photoURL: photo
+
+        updateProfile(result.user, {
+          displayName: name,
+          photoURL: photo
+        })
+
+        console.log(result.user)
+
       })
+      .catch(error => console.log(error));
 
-      console.log(result.user)
-
-    })
-    .catch(error=>console.log(error));
-  
     toast.success('Registration Successful!', {
       position: "top-right",
       autoClose: 5000,
@@ -92,52 +93,55 @@ const Registration = () => {
       draggable: true,
       progress: undefined,
       theme: "light",
-      });
+    });
   }
 
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col">
-        <div className="text-center">
-          <h1 className="text-5xl font-bold">Register</h1>
+    <>
+    <Navbar></Navbar>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col">
+          <div className="text-center">
+            <h1 className="text-5xl font-bold">Register</h1>
 
-        </div>
-        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form onSubmit={handleSignUp} className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Name</span>
-              </label>
-              <input type="text" name="name" placeholder="name" className="input input-bordered" required />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Photo URL</span>
-              </label>
-              <input type="text" name="photo" placeholder="Photo" className="input input-bordered" required />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input type="email" name="email" placeholder="email" className="input input-bordered" required />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input type="password" name="password" placeholder="password" className="input input-bordered" required />
-              
-              <div>Already have an account? <Link to='/login'>Login</Link></div>
-            </div>
-            <div className="form-control mt-6">
-              <button  className="btn btn-primary">REGISTER</button>
-              <ToastContainer />
-            </div>
-          </form>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleSignUp} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input type="text" name="name" placeholder="name" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo URL</span>
+                </label>
+                <input type="text" name="photo" placeholder="Photo" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+
+                <div>Already have an account? <Link to='/login'>Login</Link></div>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">REGISTER</button>
+                <ToastContainer />
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
